734a7f030945819104fcbfa3f2c5e3e75441020b
Miguel Diaz <71565450+libypur2k20@users.noreply.github.com>
2023-05-07 21:34:48 +0200

    CQRS for GetAllBrands on Catalog.Application project

---
+++ Catalog/Catalog.Application/Handlers/GetAllBrandsHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetAllBrandsHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+    {
+        private readonly IBrandRepository brandRepository;
+        private readonly IMapper mapper;
+
+        public GetAllBrandsHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            this.brandRepository = brandRepository;
+            this.mapper = mapper;
+        }
+
+
+        public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+        {
+            var brandList = await brandRepository.GetAllBrands();
+            var brandResponseList = mapper.Map<IList<BrandResponse>>(brandList);
+            return brandResponseList;
+        }
+    }
+}

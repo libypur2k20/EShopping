853413d7d8fb01bae085f9176fd1daa0e8a6d3d1
Miguel Diaz <71565450+libypur2k20@users.noreply.github.com>
2023-05-08 20:30:06 +0200

    CQRS for Get Product By Id

---
+++ Catalog/Catalog.Application/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, ProductResponse>
+    {
+        private readonly IProductRepository productRepository;
+
+        public GetProductByIdHandler(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public async Task<ProductResponse> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await productRepository.GetProduct(request.Id);
+            var response = ProductMapper.Mapper.Map<ProductResponse>(product);
+            return response;
+        }
+    }
+}
